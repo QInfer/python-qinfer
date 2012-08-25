0cbac45cc1ae65771ef609fbcf0cc939cdc4b73a
Christopher Granade <cgranade@cgranade.com>
2012-08-24 20:28:05 -0400

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='QInfer',
+    version='0.1a1',
+    url='https://github.com/csferrie/python-qinfer',
+    author='Chris Ferrie and Chris Granade',
+    author_email='csferrie@gmail.com',
+    package_dir={'': 'src'},
+    packages=['qinfer']
+)
