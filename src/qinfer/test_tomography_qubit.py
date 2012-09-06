0b8922e3c3b4973566453c94d98de1398b0c671e
Chris Ferrie <csferrie@gmail.com>
2012-09-05 21:57:20 -0400

    Added header/license info to test_tomography_qubit.py

--- src/qinfer/test_tomography_qubit.py
+++ src/qinfer/test_tomography_qubit.py
@@ -1,9 +1,28 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 29 18:53:24 2012
+##
+# SMC.py: Tomgraphic models module
+##
+# © 2012 Chris Ferrie (csferrie@gmail.com) and
+#        Christopher E. Granade (cgranade@gmail.com)
+#     
+# This file is a part of the Qinfer project.
+# Licensed under the AGPL version 3.
+##
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
 
-@author: csferrie
-"""
 
 ## FEATURES ####################################################################
 
@@ -11,16 +30,8 @@ from __future__ import division
 
 ## IMPORTS #####################################################################
 
-import getpass
-
-import sys
-sys.path.append({
-    'csferrie': 'C:/Users/csferrie/Documents/GitHub/python-qinfer/src/',
-    'cgranade': '/home/cgranade/academics/software-projects/python-qinfer/src/'
-}[getpass.getuser()])
-
 import numpy as np
-from qinfer import tomography, smc
+import tomography, smc
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import time
