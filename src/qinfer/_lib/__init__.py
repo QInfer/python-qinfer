b742cada379bb7549566096d1272a6c42bd8739f
Chris Granade <cgranade@cgranade.com>
2013-01-03 13:30:22 -0500

    Added external libraries folder.

---
+++ src/qinfer/_lib/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##
+# _lib/: Copies of external libraries used by QInfer, such as docopt.
+#        Note that all other files in this directory are copyrighted and
+#        licensed as described within each file, or by the corresponding
+#        LICENSE and/or COPYING files.
+##
+# © 2012 Chris Ferrie (csferrie@gmail.com) and
+#        Christopher E. Granade (cgranade@gmail.com)
+#     
+# This file is a part of the Qinfer project.
+# Licensed under the AGPL version 3.
+##
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
