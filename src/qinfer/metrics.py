Fixed bugs in Shannon entropy, KL divergence and HT circuit.
A bug in tomography.HTCircuitModel meant that models were always
considered valid, causing resampling to occasionally produce
invalid models for that example. This in turn caused
negativity in the weights after updating.

src/qinfer/metrics.py
@@ -75,7 +75,7 @@ def rescaled_distance_mtx(p, q):
     
     # Because the modelparam axis is last in each of the three cases, we're
     # good as far as broadcasting goes.
-    delta = la.sqrt(p.model.Q) * (
+    delta = np.sqrt(p.model.Q) * (
         p.particle_locations[:, np.newaxis, :] -
         q.particle_locations[np.newaxis, :, :]
     )

src/qinfer/smc.py
@@ -235,6 +235,8 @@ class SMCUpdater(object):
 
         if check_for_resample:
             self._maybe_resample()
+            
+        assert np.all(self.particle_weights >= 0)
 
     def batch_update(self, outcomes, expparams, resample_interval=5):
         r"""
@@ -413,9 +415,10 @@ class SMCUpdater(object):
         return np.dot(tot_like.T, rescale_var)
         
     def est_entropy(self):
-        return -np.sum(np.log(self.particle_weights) * self.particle_weights)
+        nz_weights = self.particle_weights[self.particle_weights > 0]
+        return -np.sum(np.log(nz_weights) * nz_weights)
         
-    def est_kl_divergence(self, other, kernel=None, epsilon=1e-6):
+    def est_kl_divergence(self, other, kernel=None, epsilon=1e-2):
         # TODO: document.
         if kernel is None:
             kernel = scipy.stats.norm(loc=0, scale=1).pdf

src/qinfer/tomography.py
@@ -103,6 +103,7 @@ class HTCircuitModel(Model):
         self.n_qubits = n_qubits
         self.n_had    = n_had
         self.f        = f
+        super(HTCircuitModel, self).__init__()
     
     ## PROPERTIES ##
     
@@ -122,7 +123,7 @@ class HTCircuitModel(Model):
     
     @staticmethod
     def are_models_valid(modelparams):
-        return np.logical_and(modelparams.all(axis=1) >= 0,modelparams.all(axis=1) <= 1)
+        return np.logical_and(modelparams >= 0, modelparams <= 1).all(axis=1)
     
     def n_outcomes(self, expparams):
         return 2