a38043352018daf20d387f36f65c21ad15a078a5
Yuval Rishu Sanders <ysanders@Arahant.(none)>
2014-03-19 11:35:59 -0400

    Added docstring to LogNormalDistribution, and included default parameters of mu=0, sigma=1.

--- src/qinfer/distributions.py
+++ src/qinfer/distributions.py
@@ -263,11 +263,19 @@ class SlantedNormalDistribution(Distribution):
         return self._ranges[:, 0] +self._weight*z+np.random.rand(n)*self._ranges[:, 1];
 
 class LogNormalDistribution(Distribution):
-    def __init__(self, mu, sigma):
-        self.mu = mu
-        self.sigma = sigma
+    """
+    Log-normal distribution.
+    
+    :param numeric mu: Location parameter, set to 0 by default.
+    :param numeric sigma: Scale parameter, set to 1 by default.
+                          Must be strictly greater than zero.
+    """
+    
+    def __init__(self, mu=0, sigma=1):
+        self.mu = mu # lognormal location parameter
+        self.sigma = sigma # lognormal scale parameter
         
-        self.dist = st.lognorm(sigma,0,mu)
+        self.dist = st.lognorm(sigma,0,mu) # scipy distribution location = 0
 
     @property
     def n_rvs(self):
