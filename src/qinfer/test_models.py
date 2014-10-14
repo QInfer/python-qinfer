b36f0cd4c70f6a486e882e336b8f5c9647a78cfc
Christopher Granade <cgranade@cgranade.com>
2014-10-14 01:13:08 -0400

    added __all__ to test_models.

--- src/qinfer/test_models.py
+++ src/qinfer/test_models.py
@@ -23,11 +23,19 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
-## FEATURES ##
+## FEATURES ##################################################################
 
 from __future__ import division # Ensures that a/b is always a float.
 
-## IMPORTS ##
+## EXPORTS ###################################################################
+
+__all__ = [
+    'SimplePrecessionModel',
+    'NoisyCoinModel',
+    'NDieModel'
+]
+
+## IMPORTS ###################################################################
 
 import numpy as np
 
@@ -35,7 +43,7 @@ from utils import binomial_pdf
 
 from abstract_model import Model, DifferentiableModel
     
-## CLASSES #####################################################################
+## CLASSES ###################################################################
 
 class SimplePrecessionModel(DifferentiableModel):
     r"""
