2e1b6795ff78d43292f78f56da240723732776fc
Steven Casagrande <scasagrande@galvant.ca>
2016-04-03 22:15:30 -0400

    Add division features

--- src/qinfer/distributions.py
+++ src/qinfer/distributions.py
@@ -25,6 +25,7 @@
 
 ## IMPORTS ###################################################################
 
+from __future__ import division
 from __future__ import absolute_import
 import numpy as np
 import scipy.stats as st
