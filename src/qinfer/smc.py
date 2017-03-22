19f2864587ba4dda16233be474d9b58096798cf1
Ian Hincks <ian.hincks@gmail.com>
2017-03-22 12:33:01 -0400

    added default n_particles to LuiWest resampler

--- src/qinfer/smc.py
+++ src/qinfer/smc.py
@@ -145,7 +145,7 @@ class SMCUpdater(Distribution):
             self.resampler = qinfer.resamplers.LiuWestResampler(a=resample_a)
         else:
             if resampler is None:
-                self.resampler = qinfer.resamplers.LiuWestResampler()
+                self.resampler = qinfer.resamplers.LiuWestResampler(n_particles=n_particles)
             else:
                 self.resampler = resampler
 
